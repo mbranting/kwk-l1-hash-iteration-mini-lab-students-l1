f3897b6952aee965fa6a5568ed678b82775e5e82
mbranting <mckenna.branting@gmail.com>
2018-06-06 20:30:58 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -46,7 +46,7 @@ def iterate_through_hash
     puts "The #{date} summer olympics took place in #{city}."
 end
 puts iterate_through_hash
-
+end
 # def iterate_through_keys
   # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
   # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
