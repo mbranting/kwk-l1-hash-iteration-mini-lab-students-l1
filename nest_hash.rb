42ae5a41264da5384b1298d3aa2ef8c1fa033b6c
mbranting <mckenna.branting@gmail.com>
2018-06-06 21:23:20 +0000

    automatically backed up by learn

---
+++ nest_hash.rb
@@ -0,0 +1,7 @@
+closet = {
+  "tops" => ["rain jacket", "T-shirt", "hoodie", "cardigan"]
+  "bottoms" => ["jeans", "skirts", "capris", "shorts"]
+  "shoes" => ["nike", "vans", "flip flops", "heels", "adidas"]
+}
+#just want tops
+puts closet("tops")
