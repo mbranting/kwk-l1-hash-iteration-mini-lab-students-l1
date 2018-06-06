6e1f58d19330f363909c9a323d140b638f835cca
mbranting <mckenna.branting@gmail.com>
2018-06-06 20:46:37 +0000

    automatically backed up by learn

--- nested_array.rb
+++ nested_array.rb
@@ -7,3 +7,9 @@
 #   puts closet[0] #shows only things in the first array
 #   # .class tells you what type of data you have 
   
+  tops = ["rain jacket", "cardigan", "blazer"]
+  bottoms = ["jeans", "skirt", "capris"]
+  shoes = ["sneakers", "heels", "flats"]
+  
+  
+puts closet [tops, bottoms, shoes]
