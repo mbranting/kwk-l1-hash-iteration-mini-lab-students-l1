02799d7de26951364d568f117d9374a584286a48
mbranting <mckenna.branting@gmail.com>
2018-06-06 20:12:37 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -26,6 +26,7 @@ create_olympics_hash = {
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
   
   create_olympics_hash["Atlanta"]= "1996"
+  puts create_olympics_hash
 end
 puts add_a_key_value_pair
 
