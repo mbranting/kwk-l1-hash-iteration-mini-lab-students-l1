56b08378ee8e9e3288b6337672333f4bb33b0575
mbranting <mckenna.branting@gmail.com>
2018-06-06 20:39:30 +0000

    automatically backed up by learn

--- nested_array.rb
+++ nested_array.rb
@@ -1,5 +1,8 @@
 closet = [
   ["rain jacket", "cardigan","blazer"],
-    ["jeans", "skirt", "capris"],
-    ["sneakers", "heels", "flats"]
-    ]
+  ["jeans", "skirt", "capris"],
+  ["sneakers", "heels", "flats"]
+  ]
+  
+  closet.[0].class
+    
